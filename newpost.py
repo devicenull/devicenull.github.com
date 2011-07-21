563473dd0792ff3de3e9dde3fd6641f08117d20d
Brian Rak <dn@devicenull.org>
2011-07-20 20:48:03 -0400

    Add first pxe post, newpost script

---
+++ newpost.py
@@ -0,0 +1,21 @@
+import time, subprocess 
+
+title = raw_input("Title:")
+
+filename = "_posts\\%s-%s.textile" % (time.strftime('%Y-%m-%d'), title.replace(" ","-").lower().strip())
+
+print "filename: %s" % filename
+
+f = open(filename,"w")
+
+f.write(
+"""---
+layout: post
+title: %s
+---
+
+""" % title)
+
+f.close()
+
+subprocess.Popen("D:\\Apps\\Notepad++\\notepad++.exe %s" % filename)
